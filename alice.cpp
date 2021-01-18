712321c7d2aa70be80bf9fdd11f757fec31388ea
takenX10 <theassasinsheepmc@gmail.com>
2021-01-18 20:10:12 +0100

    Implementate alcune funzioni base
    Implementate:
    spawn (una parte)
    pulisci_lista_nemici
    death
    newid
    alcuni metodi della classe Entity
    un idea dei calcola_movimento
    
    @Apo @takenX10

--- alice.cpp
+++ alice.cpp
@@ -1,12 +1,34 @@
 /*
 @ Author: Alice Benatti
 
+- uno che si occupa del game design (
+    aumento di livello, statistiche e percentuali), 
+    del main, 
+    game over e classifica (8)
+- i bonus li implementa quello che ha meno roba da fare (ce ne accorgeremo sul momento)
+
+- schermata di inizio (premi invio per iniziare ecc....)
+- classifica
+- schermata game over
 */
 #include<iostream>
 using namespace std;
 
+void StartScreen();
+
 int main(){
-    
-    cout << "^.^"<< endl;
+   StartScreen();
     return 0;
+}
+
+void StartScreen(){
+    char key;
+    char title;
+    cout << title;
+    cout << "press enter to play";
+    cout << "press C to view the leaderboard";
+    cin << key;
+    if (key == 10) goto "play";
+    else if (key == 67 || key == 99) goto "classifica";
+    else cout << "ERROR: u've digited wrong u.u";
 }
