dac607da4fa6da768486588df088f24535552afa
takenX10 <theassasinsheepmc@gmail.com>
2021-01-31 17:58:50 +0100

    Modifiche importanti print mappa
    test.cpp:
    Implementato il multithreading nel codice di @alexlorenzato .
    tempCodeRunnerFile.cpp:
    create due funzioni di prova:
    movecursor, che ti permette di muovere il cursore nelle cordinate che desideri, e findchar, che ti restituisce il carattere nella console alla posizione desiderata
    
    todo: modificare il printmappa in modo da avere un refresh piu uniforme tramite movecursore e finchar

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,45 @@
+/*
+@ Author: Alessandro Frau
 
+*/
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+
+
+char findchar(int column, int line){
+    char buf[1];
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    DWORD num_read;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    ReadConsoleOutputCharacter(hConsole, (LPTSTR) buf, 1, coord, (LPDWORD) &num_read);
+    return buf[0];
+}
+
+void movecursor(int column, int line){
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (!SetConsoleCursorPosition(hConsole, coord)){
+        cout<<"oh rabbia, un errore!"<<endl;
+    }
+}
+
+int main(){
+    cout<<"123456789"<<endl;
+    cout<<"123456789"<<endl;
+    cout<<"123456789"<<endl;
+    cout<<"123456789"<<endl;
+    cout<<findchar(1,1);
+    system("PAUSE");
+    movecursor(1, 1);
+    cout<<"ao";
+    movecursor(5, 1);
+    cout<<"sa";
+    getch();
+    return 0;
+}
