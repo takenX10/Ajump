0b4e0026ddea64146d38c250e2d68359eead53bb
takenX10 <frau.alessandro01@gmail.com>
2021-04-08 19:20:04 +0200

    Modifiche parziali riguardo la lista di entita
    Questo commit e' fatto per condividere questo codice parziale, va ancora testato e dovrei farne un altro piu tardi completo, le funzioni non sono ancora complete

--- Alessandro/Alessandro.cpp
+++ Alessandro/Alessandro.cpp
@@ -3,123 +3,91 @@
 
 */
 #include<iostream>
-#include <ncurses.h>
+#include "funzioni_alex/Mappa.h"
+#include "funzioni_alex/Player.h"
 using namespace std;
 
-struct listaNemici{
-    class Enemy nemico;
-    struct listaNemici *next;
+class Nemico{
+    public:
+        int x;
+        int y;
+        
 };
 
-typedef struct listaNemici* plistaNemici;
-
-struct position{
-    int x;
-    int y;
-};
-
-// Funzione che si occupa di rimuovere un nemico dalla lista dei nemici
-plistaNemici death(plistaNemici list, int id){
-    plistaNemici aux = list;
-    plistaNemici prev = NULL;
-    while(aux->nemico.id != id && aux != NULL){
-        prev = aux;
-        aux = aux->next;
-    }
-    if(prev == NULL){ // l'elemento da eliminare e' il primo
-        list = list->next;
-        delete(aux);
-    }else if(aux != NULL){
-        prev->next = aux->next;
-        delete(aux);
-    }
-    return list;
-}
-
-// Funzione che si occupa di generare un nuovo id valido per la classe
-// L'id corrente viene passato per riferimento
-int newid(int &id_backup){
-    id_backup++;
-    return id_backup;
-}
-
-
-// Funzione che si occupa di pulire la lista dei nemici
-// Da chiamare alla fine di ogni partita
-
-plistaNemici pulisci_lista_nemici(plistaNemici head){ //perchè non void (?)
-    plistaNemici aux = head;
-    while(head != NULL){
-        aux = aux->next;
-        delete(head);
-        head = aux;
-    }
-    return head;
-}
-
-/****** Manca roba da implementare *************/
-//funzione che si occupa di far "spawnare un nemico"
-plistaNemici spawn(plistaNemici *head, int &id_backup, int vita, int danno, int tipo){
-    plistaNemici aux = new struct listaNemici;
-    int id = newid(id_backup);
-    position pos;
-    // dobbiamo calcolare la posizione in cui farlo spawnare, attendiamo la mappa di alex
-    aux->nemico.setStats(vita, danno, pos, id);
-}
-
-class Entity{ // classe madre
-protected:
-
-public:
-    int vita;
-    int danno;
-    int id;
-    position posEntity;
-    Entity(position pos, int vita = 0, int danno = 0, int id=0){
-        this->vita = vita;
-        this->danno = danno;
-        this->posEntity.x = pos.x;
-        this->posEntity.x = pos.y;
-        this->id = id;
-    }
-    // presuppone che il movimento sia possibile
-    bool move(position newPos){
-        this->posEntity.x = newPos.x;
-        this->posEntity.y = newPos.y;
-    }
-
-    void setStats(int vita, int danno, position pos, int id){
-        this->vita = vita;
-        this->danno = danno;
-        this->posEntity = pos;
-        this->id = id;
-    }
-};
-
-class Enemy: public Entity{
-protected:
-    char type;
-    char boss;
-public:
-
+struct lista_entita{
+    Nemico entity;
+    bool can_move;
+    int move_direction;
+    struct lista_entita *next;
+    struct lista_entita *prev;
 
 };
+typedef lista_entita *ptr_lista_entita;
 
-class Player: public Entity{
+class gestione_entita{
+    protected:
+        ptr_lista_entita head;
+    public:
+        gestione_entita(void){
+            this->head = NULL;
+        }
+        // aggiunta in testa, quindi se si aggiungono solo nuovi nemici la lista
+        // risulta sempre ordinata per le righe
+        void aggiungi_nemico(Nemico enemy){
+            ptr_lista_entita new_enemy = new lista_entita;
+            new_enemy->entity = enemy;
+            if (this->head != NULL){
+                this->head->prev = new_enemy;
+            }
+            new_enemy->prev = NULL;
+            new_enemy->next = this->head;
+            this->head = new_enemy;
+        }
 
+        // del player in realta ci interessano solo le coordinate
+        // aggiorna il valore della variabile "can_move" di ogni entita della lista
+        void si_puo_muovere(Mappa *map, Player *player){
+            ptr_lista_entita tmp = this->head;
+            if(tmp != NULL){
+                int playerX = player->getX();
+                while(tmp->next != NULL && tmp->next->entity.x < playerX){
+                    tmp = tmp->next;
+                }
+                ptr_lista_entita left = tmp;
+                int fdir = 1;
+                ptr_lista_entita right;
+                ptr_lista_entita first;
+                ptr_lista_entita second;
+                if (tmp->next == NULL){
+                    first = left;
+                    second = NULL;
+                    right = NULL;
+                }else if(playerX == tmp->next->entity.x){
+                    right = tmp->next->next;
+                }else{
+                    right = tmp->next;
+                }
+                if(right != NULL){
+                    if(left->entity.y < right->entity.y){
+                        first = left;
+                        second = right;
+                    }else{
+                        first = right;
+                        fdir = 0;
+                        second = left;
+                    }
+                }
+                while(first != NULL){
+                    if((fdir == 1? first->next:first->prev)->entity.x == playerX){
+                        first->move_direction = DOWN;
+                    }else{
+                        first->move_direction = (fdir == 1? DOWN_RIGHT: DOWN_LEFT);
+                    }
+                }
+                
+            }
+        }
+        void muovi_nemici(Mappa *map){
+
+        }
 };
-
-
-//in attesa della mappa di Alessio
-position calcola_movimento_nemico(class Enemy c, struct mappa *head){
-    position pos;
-
-    c.move(pos);
-    return pos;
-}
-
-position calcola_movimento_player(class Player c, struct mappa *head){
-    position pos;
-
-    return pos;
-}
